52fc61b4ed519b54c67e343b68d70f0866457730
theodore-thormann <50964270+theodore-thormann@users.noreply.github.com>
2022-07-11 14:50:31 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='booklover',
+      version='0.1',
+      description='A booklover class from hw09',
+      url='http://github.com/theodore-thormann/book_lover_repo',
+      author='Theo Thormann',
+      author_email='nbx5kp@virginia.edu',
+      license='MIT',
+      packages=['booklover'])
